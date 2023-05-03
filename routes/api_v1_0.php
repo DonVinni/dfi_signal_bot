d568cf8e78ca2157d1f2d8d99a93eb74dc25ab0a
Adrian <322735+adrian-schnell@users.noreply.github.com>
2023-05-03 23:09:05 +0200

    remove masternode health (#124)
    * remove route
    
    * remove health settings
    
    * update README.md
    
    * update job, webhook controller, conversations and analyser
    
    * update translation

--- routes/api_v1_0.php
+++ routes/api_v1_0.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Api\v1_0\MasternodeHealthWebhookController;
 use App\Api\v1_0\ServerSyncController;
 use App\Api\v1_0\StatisticController;
 
@@ -10,5 +9,3 @@ Route::get('statistics/all', [StatisticController::class, 'getAll'])
     ->name('statistics.all');
 Route::get('statistics/last_week', [StatisticController::class, 'getLastWeek'])
     ->name('statistics.last_week');
-Route::post('webhook/masternode_health', [MasternodeHealthWebhookController::class, 'receiveWebhook'])
-    ->name('webhook.masternode_health');
